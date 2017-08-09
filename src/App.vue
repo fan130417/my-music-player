ebf9c7a2225bf774c2c1c069b3f8e0a3a0dfe3cd
fanjunjie <fanjunjie@fanjunjiedeMacBook-Pro.local>
2017-08-09 09:55:07 +0800

    优化播放器全屏时路由处理

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,32 @@
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/m-header/m-header'
+  import { mapGetters, mapMutations } from 'vuex'
   import Tab from 'components/tab/tab'
   import Player from 'components/player/player'
+
   export default {
+    computed: {
+      ...mapGetters([
+        'fullScreen'
+      ])
+    },
+    methods: {
+      ...mapMutations({
+        setFullScreen: 'SET_FULL_SCREEN'
+      })
+    },
+    watch: {
+      $route: {
+        handler: function () {
+          if (this.fullScreen) {
+            this.setFullScreen(!this.fullScreen)
+            this.$router.forward()
+            return
+          }
+        }
+      }
+    },
     components: {
       MHeader,
       Tab,
